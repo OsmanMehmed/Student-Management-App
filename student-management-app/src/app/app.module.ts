22e212de4e03cb989da58abd929857900e2a15e6
Osman Mehmed <osman@devanddel.com>
2022-04-08 14:58:54 +0200

    Añadida pantalla sidenav

--- student-management-app/src/app/app.module.ts
+++ student-management-app/src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaAlumnosComponent } from './features/lista-alumnos/lista-alumnos.component';
+import { MainComponent } from './features/main/main.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { AlumnoCardComponent } from './features/alumno-card/alumno-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListaAlumnosComponent,
+    AlumnoCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
